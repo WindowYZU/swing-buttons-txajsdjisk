2e70c68cc1e29f74b1be9d495ee78f89c521e80b
user <user@007>
2018-10-02 15:59:51 +0800

    PP

--- src/main/java/lendle/courses/wp/buttonsamples/RadioButtonSample.java
+++ src/main/java/lendle/courses/wp/buttonsamples/RadioButtonSample.java
@@ -10,6 +10,7 @@ import javax.swing.ButtonGroup;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JRadioButton;
+import javax.swing.JToggleButton;
 import javax.swing.WindowConstants;
 
 /**
@@ -28,11 +29,18 @@ public class RadioButtonSample {
         frame.setLayout(new FlowLayout());
         JRadioButton radio1 = new JRadioButton("radio1");
         JRadioButton radio2 = new JRadioButton("radio2");
-        
+        JCheckBox k=new JCheckBox("check1");
+        JToggleButton j=new JToggleButton("toggle1");
         frame.add(radio1);
         frame.add(radio2);
+        frame.add(k);
+        frame.add(j);
         //建立 ButtonGroup，把 radio1, radio2 加到 ButtonGroup
-        
+         ButtonGroup group=new ButtonGroup();
+         group.add(radio1);
+         group.add(radio2);
+         group.add(k);
+         group.add(j);
         ////////////////////////////////////////////////////
         
         frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
